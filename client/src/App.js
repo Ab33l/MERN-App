b66757280d609e8617e9e87deac0f87b05ed2866
Ab33l <abell.wandili@strathmore.edu>
2021-04-01 14:31:48 +0300

    Basic Structure and functional connection and communication between Frontend, Backend and the Database. Basic levels of CRUD functionality achieved.

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, {useEffect} from "react";
+import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import memoir from './images/memoir.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './styles';
+import {useDispatch} from 'react-redux';
+import {getPosts} from './actions/posts';
+
+const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+    
+    return(
+        <Container maxwidth = "lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memoir</Typography>
+                <img className={classes.image} src={memoir} alt="Memoir" height="60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    )
+}
+
+export default App;
